8c1b498e059fa0ee5190f75631b503446870e88c
ElMonje1 <juan-135v@hotmail.com>
2025-03-01 00:39:25 -0600

    Ok Confirmación 2 Monje
    Integración 2

--- app/src/main/java/com/example/proyecto2adminmonje/MainActivity.java
+++ app/src/main/java/com/example/proyecto2adminmonje/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.proyecto2adminmonje;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {//Inicia clase
+    public Button btnVenta, btnContacto, btnProductos;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//Inicia metodo onCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Integracion de xml-java
+        btnVenta = findViewById(R.id.btnVenta);
+        btnContacto = findViewById(R.id.btnContacto);
+        btnProductos = findViewById(R.id.btnProductos);
 
+        //Evento para abrir la nueva ventana
+        btnVenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirVenta = new Intent(getApplicationContext(), Venta.class);
+                startActivity(abrirVenta);
+            }
+        });
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirContacto = new Intent(getApplicationContext(), Contacto.class);
+                startActivity(abrirContacto);
+            }
+        });
+        btnProductos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirProducto = new Intent(getApplicationContext(), Producto.class);
+                startActivity(abrirProducto);
+            }
+        });
 
-    }
-}
+    }//Termina el metodo onCreate
+}//Termina clase
